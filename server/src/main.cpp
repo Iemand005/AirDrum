fe51e37e5af0cf494907dc77d336b519ed2f4780
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-12 11:28:50 +0200

    Actually move now

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -45,32 +45,6 @@ const int threshold = 10;
 
 const bool applyLowPassFilter = true;
 
-/**
- * @brief Vector of 3 16bit integers
- */
-struct Vec3I16 {
-    int16_t x, y, z;
-
-    Vec3I16 operator-(const Vec3I16& other) const {
-        return {x - other.x, y - other.y, z - other.z};
-    }
-};
-
-/**
- * @brief Vector of 3 floats
- */
-struct Vec3 {
-    float x, y, z;
-
-    void normalize() {
-        // float length = std::sqrt(x * x + y * y + z * z);
-        // if (length > 0.0f) {
-        //     x /= length;
-        //     y /= length;
-        //     z /= length;
-        // }
-    }
-};
 
 
 // Three-axis baseline values for resting position
