a18cad5ba952ac755eafb700fb1c9d58b10bbf1c
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-13 11:31:14 +0200

    nothing

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -41,7 +41,7 @@ VCC -> resistor 220 Ohm -> Arduino D3
 #define REG_WHO_AM_I 0x75
 
 // Threshold for movement detection (tune as needed)
-const int threshold = 1000;
+const int threshold = 1;
 
 const bool applyLowPassFilter = true;
 
