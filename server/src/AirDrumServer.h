3367d64811164e1c7ae92b9a1e389d92fdda681b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-13 15:02:18 +0200

    wifi conenct unciton

--- server/src/AirDrumServer.h
+++ server/src/AirDrumServer.h
@@ -11,6 +11,20 @@ class AirDrumServer {
     WiFi.mode(WIFI_OFF);
   }
 
+  bool wifiConnect(const char *ssid, const char *password) {
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("Wi-Fi Connected!");
+    Serial.print("IP-adres of: ");
+    Serial.println(WiFi.localIP());
+  }
+
   void disableBluetooth() {
     
   }
