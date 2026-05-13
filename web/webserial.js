c9e6ba26c5b58a9253a1f93171387171f46c8013
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-13 10:01:53 +0200

    Port selection and open

--- web/webserial.js
+++ web/webserial.js
@@ -3,21 +3,27 @@ const button = document.getElementById("button");
 
 navigator.serial.addEventListener("connect", (e) => {
   // Connect to `e.target` or add it to a list of available ports.
+  console.log("Connected", e);
+  // console.log(e);
 });
 
 navigator.serial.addEventListener("disconnect", (e) => {
+  console.log("disconn", e)
   // Remove `e.target` from the list of available ports.
 });
 
 navigator.serial.getPorts().then((ports) => {
+  console.log("gor ports", ports)
   // Initialize the list of available ports with `ports` on page load.
 });
 
 button.addEventListener("click", () => {
-  const usbVendorId = 0xabcd;
+  
   navigator.serial
-    .requestPort({ filters: [{ usbVendorId }] })
-    .then((port) => {
+    .requestPort()
+    .then(async port => {
+      // port
+      await port.open({ baudRate: 9600 });
       // Connect to `port` or add it to the list of available ports.
     })
     .catch((e) => {
