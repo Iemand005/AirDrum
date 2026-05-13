d96b008bdbe42485a8a55a36225594d9a9f1a139
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-13 15:47:51 +0200

    socket code

--- web/websocket.js
+++ web/websocket.js
@@ -2,5 +2,24 @@
 const button = document.getElementById("button");
 
 button.onclick = () => {
-  const socket = new WebSocket("ws://ws://10.25.241.136/ws")
+  const socket = new WebSocket("ws://10.25.241.136/ws");
+
+  socket.onopen = () => {
+    console.log('CONNECTED TO AirDrumServer!');
+    
+    console.log('Testbericht sending: HIT_SNARE');
+    socket.send('HIT_SNARE');
+  };
+
+  socket.onmessage = (event) => {
+      console.log('Message receivd', event.data);
+  };
+
+  socket.onclose = () => {
+      console.log('closedrt');
+  };
+
+  socket.onerror = (error) => {
+      console.error('Werror gone shit', error);
+  };
 };
