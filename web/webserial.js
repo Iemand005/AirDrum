b2cc3777d6e281c8b9679380b3065fbad5d95c41
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-13 15:45:21 +0200

    share code

--- web/webserial.js
+++ web/webserial.js
@@ -17,26 +17,6 @@ navigator.serial.getPorts().then((ports) => {
   console.log("gor ports", ports)
 });
 
-function playBeep(frequency = 1000, duration = 200, volume = 0.8) {
-    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    
-    const oscillator = audioCtx.createOscillator();
-    
-    const gainNode = audioCtx.createGain();
-
-    oscillator.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
-
-    oscillator.type = 'sine';
-    oscillator.frequency.value = frequency;
-
-    gainNode.gain.setValueAtTime(volume, audioCtx.currentTime);
-    gainNode.gain.exponentialRampToValueAtTime(0.00001, audioCtx.currentTime + duration / 1000);
-
-    oscillator.start(audioCtx.currentTime);
-    oscillator.stop(audioCtx.currentTime + duration / 1000);
-}
-
 async function startReadingLines(port, callback) {
 
   console.log("port reached here!", port)
