71ec50b862822531b2b0fa0c42eb43574e8f744d
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-13 15:33:19 +0200

    lambda

--- server/src/SocketServer.h
+++ server/src/SocketServer.h
@@ -7,9 +7,17 @@
 
 class SocketServer {
 
-  AsyncWebServer server(80);
-  AsyncWebSocket ws("/socket");
+  AsyncWebServer server;
+  AsyncWebSocket ws;
 
+  void startListener() {
+    ws.onEvent([](AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
+
+    });
+    server.addHandler(&ws);
+    
+    server.begin();
+  }
 };
 
 #endif
