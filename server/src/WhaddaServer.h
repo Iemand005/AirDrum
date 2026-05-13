0f3233f69a7b286f4439dddddd40132451956377
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-13 16:10:28 +0200

    this is a bit like it

--- server/src/WhaddaServer.h
+++ server/src/WhaddaServer.h
@@ -1,8 +1,23 @@
 
 #ifndef WhaddaServer_h
+#define WhaddaServer_h
+
+#include <RCSwitch.h>
 
 class WhaddaServer {
+  int code = 0;
+  RCSwitch mySwitch;
 
+public:
+
+  WhaddaServer() : mySwitch() {}
+
+  void sendValue(int value) {
+      unsigned long combined = (code << 8) | value;
+
+      this->mySwitch.send(combined, 24);
+      code++;
+  }
 };
 
 #endif
