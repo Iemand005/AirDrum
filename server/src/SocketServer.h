b8d1b77497362fbcfd81eee0a48781ca6660337b
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-13 15:35:27 +0200

    wesbocket shit

--- server/src/SocketServer.h
+++ server/src/SocketServer.h
@@ -10,9 +10,18 @@ class SocketServer {
   AsyncWebServer server;
   AsyncWebSocket ws;
 
+public:
+  SocketServer() : server(80), ws("/ws") {}
   void startListener() {
-    ws.onEvent([](AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
-
+    ws.onEvent([this](AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len) {
+      if (type == WS_EVT_DATA) {
+        data[len] = 0;
+        String message = (char*)data;
+        
+        Serial.print("WebSocket message! ");
+        Serial.println(message);
+        // 8
+      }
     });
     server.addHandler(&ws);
     
