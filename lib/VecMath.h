073c59e461ad69209593f024b344cebad399f087
Lasse <Lasse@LASSES-LAPTOP>
2026-05-22 11:20:54 +0200

    sticks

--- lib/VecMath.h
+++ lib/VecMath.h
@@ -5,6 +5,21 @@
 #include <stdint.h>
 
 
+/**
+ * @brief Vector of 3 16bit integers
+ */
+struct Vec3I16 {
+    int16_t x, y, z;
+
+    Vec3I16 operator-(const Vec3I16& other) const {
+        return {x - other.x, y - other.y, z - other.z};
+    }
+
+    int magnitude() {
+        return abs(x) + abs(y) + abs(z);
+    }
+};
+
 // /**
 //  * @brief Vector of 3 16bit integers
 //  */
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -7,6 +7,7 @@
 // #include "../lib/WhaddaServer.h"
 #include <WhaddaServer.h>
 #include <SerialCodes.h>
+#include <VecMath.h>
 
 #define USE_WIFI
 // #define USE_WHADDA
@@ -67,20 +68,7 @@ int stillMomentCount = 0;
 const bool applyLowPassFilter = true;
 
 
-/**
- * @brief Vector of 3 16bit integers
- */
-struct Vec3I16 {
-    int16_t x, y, z;
 
-    Vec3I16 operator-(const Vec3I16& other) const {
-        return {x - other.x, y - other.y, z - other.z};
-    }
-
-    int magnitude() {
-        return abs(x) + abs(y) + abs(z);
-    }
-};
 
 /**
  * @brief Vector of 3 floats
