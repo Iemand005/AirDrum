fa486c84cdf158f895f8d5103dbd98998f5a211d
Lasse <Lasse@LASSES-LAPTOP>
2026-05-22 11:05:15 +0200

    print IP more recognisably

--- lib/AirDrumServer.h
+++ lib/AirDrumServer.h
@@ -32,7 +32,7 @@ public:
 
     Serial.println("");
     Serial.println("Wi-Fi Connected!");
-    Serial.print("IP-adres of: ");
+    Serial.print("IP: ");
     Serial.println(WiFi.localIP());
 
     return true;
