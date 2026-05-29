f500b9e501e62bb9b131f4a5397ee271989b3234
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-05-29 14:27:32 +0200

    json shit

--- lib/WebSocketClient.h
+++ lib/WebSocketClient.h
@@ -11,7 +11,6 @@ private:
     int _port;
     const char* _path;
     bool _wsConnected;
-    bool _stompConnected;
     bool _warnedNotConnected;
 
     static void _webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
@@ -20,36 +19,23 @@ private:
         }
     }
 
-    void sendFrame(const String& frame) {
-        // STOMP frames must be NULL-terminated, so we explicitly include the trailing '\0'.
-        _webSocket.sendTXT(frame.c_str(), frame.length() + 1);
-    }
-
     void handleEvent(WStype_t type, uint8_t * payload, size_t length) {
         switch(type) {
             case WStype_DISCONNECTED:
                 Serial.println("[WS] Verbinding verbroken! Automatische herstelpoging loopt...");
                 _wsConnected = false;
-                _stompConnected = false;
                 _warnedNotConnected = false;
                 break;
                 
             case WStype_CONNECTED:
-                Serial.println("[WS] Ruwe WebSocket verbonden. STOMP handshake starten...");
+                Serial.println("[WS] WebSocket verbonden.");
                 _wsConnected = true;
-                {
-                    String connectFrame = "CONNECT\naccept-version:1.1,1.2\nheart-beat:10000,10000\n\n";
-                    sendFrame(connectFrame);
-                }
                 break;
                 
             case WStype_TEXT: {
                 String msg((char*)payload);
-                if (msg.startsWith("CONNECTED")) {
-                    Serial.println("[WS] STOMP verbinding succesvol tot stand gebracht!");
-                    _stompConnected = true;
-                    _warnedNotConnected = false;
-                }
+                Serial.print("[WS] Ontvangen: ");
+                Serial.println(msg);
                 break;
             }
             default:
@@ -65,7 +51,6 @@ public:
         _port = port;
         _path = path;
         _wsConnected = false;
-        _stompConnected = false;
         _warnedNotConnected = false;
         WebSocketClient::instance = this;
     }
@@ -81,24 +66,19 @@ public:
     }
 
     void sendText(String payload) {
-        if (!_stompConnected) {
+        if (!_wsConnected) {
             if (!_warnedNotConnected) {
-                Serial.println("[WS] Kan bericht niet verzenden: STOMP is niet verbonden!");
+                Serial.println("[WS] Kan bericht niet verzenden: WebSocket is niet verbonden!");
                 _warnedNotConnected = true;
             }
             return;
         }
 
-        String stompFrame = "SEND\n";
-        stompFrame += "destination:/app/drum.send\n"; 
-        stompFrame += "content-length:" + String(payload.length()) + "\n\n";
-        stompFrame += payload;
-
-        sendFrame(stompFrame);
+        _webSocket.sendTXT(payload);
     }
 
     bool isConnected() {
-        return _stompConnected;
+        return _wsConnected;
     }
 };
 
