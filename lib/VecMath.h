5fa72745ff0370bf3bdce8ed585d6330a95a4f17
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-01 11:02:15 +0200

    awefawef

--- lib/VecMath.h
+++ lib/VecMath.h
@@ -27,31 +27,4 @@ struct Vec3 {
     float x, y, z;
 };
 
-// /**
-//  * @brief Vector of 3 16bit integers
-//  */
-// struct Vec3I16 {
-//     int16_t x, y, z;
-
-//     Vec3I16 operator-(const Vec3I16& other) const {
-//         return {x - other.x, y - other.y, z - other.z};
-//     }
-// };
-
-// /**
-//  * @brief Vector of 3 floats
-//  */
-// struct Vec3 {
-//     float x, y, z;
-
-//     void normalize() {
-//         // float length = std::sqrt(x * x + y * y + z * z);
-//         // if (length > 0.0f) {
-//         //     x /= length;
-//         //     y /= length;
-//         //     z /= length;
-//         // }
-//     }
-// };
-
 #endif
