b107b015ab835e94e248f0da15deed89fdb2da3f
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-06-01 11:42:00 +0200

    sensitivty

--- lib/AirDrumMain.h
+++ lib/AirDrumMain.h
@@ -92,7 +92,7 @@ const int maxInstruments = 7;
 
 
 const int gyroXdiff = 20;
-const Vec3I16 gyroThreshold = {700, 1500, 500};
+const Vec3I16 gyroThreshold = {1400, 1500, 500};
 const int gyroXIgnoreBelow = 20;
 const Vec3I16 gyroCancel = {-5, -5, -5};
 Vec3I16 gyroSum = {0, 0, 0};
